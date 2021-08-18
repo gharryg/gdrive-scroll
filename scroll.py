482ccb029df4e051f68586c8b04f8880aa606655
Harrison Golden <gharryg@mac.com>
2021-08-18 09:29:42 -0500

    v1 of major rewrite; added music

--- scroll.py
+++ scroll.py
@@ -1,84 +1,114 @@
-import os
-from time import sleep
-import pickle
-import google_auth_oauthlib
-from google.auth.transport.requests import AuthorizedSession
-from google.auth.exceptions import RefreshError
-import sys
+import argparse
+import json
+import pathlib
 import shutil
 import subprocess
-
-# https://console.developers.google.com/apis/credentials for CLIENT_ID and CLIENT_SECRET
-CLIENT_ID = 'FOO'
-CLIENT_SECRET = 'FOO'
-# https://developers.google.com/drive/api/v3/about-auth
-SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
-PARENT_ID = 'FOO'
-API_ROOT = 'https://www.googleapis.com/drive/v3'
-DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-CREDENTIALS_PATH = '%s/credentials.pickle' % DIR_PATH
-IMAGES_PATH = '%s/images' % DIR_PATH
+import sys
+from google_auth_oauthlib import get_user_credentials
+from google.auth.transport.requests import AuthorizedSession
+from google.oauth2.credentials import Credentials
 
 
-def rolling_slides():
+def main():
+    parser = argparse.ArgumentParser(description='A tool for fetching and scrolling images stored in Google Drive.')
+    parser.add_argument('--credential-store', required=True, help='Location of persistent credential store.')
+    parser.add_argument('--fallback-image', help='Locally stored image that will be displayed if unable to fetch Google Drive images.')
+    parser.add_argument('--images-parent-id', required=True, help='Google Drive parent (folder) id that contains the child images.')
+    parser.add_argument('--music-parent-id', help='Google Drive parent (folder) id that contains the child music files.')
+    args = parser.parse_args()
+
+    # https://developers.google.com/drive/api/v3/about-auth
     # https://google-auth.readthedocs.io/en/latest/user-guide.html#user-credentials
-    # https://google-auth-oauthlib.readthedocs.io/en/latest/reference/google_auth_oauthlib.html
-    # check access_token status: https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=[TOKEN]
-    # load credentials object from pickle
-    if os.path.exists(CREDENTIALS_PATH):
-        with open(CREDENTIALS_PATH, 'rb') as f:
-            credentials = pickle.load(f)
-    else:
-        # fetch new credentials
-        credentials = google_auth_oauthlib.get_user_credentials(SCOPES, CLIENT_ID, CLIENT_SECRET)
+    # Tool for checking access tokens: https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=[TOKEN]
 
-    # create google-auth requests session; this will handle token refresh and checking
-    # https://google-auth.readthedocs.io/en/latest/reference/google.auth.transport.requests.html#google.auth.transport.requests.AuthorizedSession
+    credential_path = pathlib.Path(args.credential_store)
+    if credential_path.is_file():
+        with open(credential_path) as stream:
+            credentials_file = json.load(stream)
+
+        # When we create the Credentials object later using **kwargs, the object behaves odd when expiry is set at initialization so we delete it.
+        if 'expiry' in credentials_file:
+            del credentials_file['expiry']
+    else:
+        print('ERROR: Credential store provided is not a file')
+        # TODO: show fallback image
+        sys.exit(1)
+
+    # Assume this is the first run and we need to go through the OAuth workflow.
+    if 'token' not in credentials_file:
+        try:
+            # https://google-auth-oauthlib.readthedocs.io/en/latest/reference/google_auth_oauthlib.html#google_auth_oauthlib.get_user_credentials
+            credentials = get_user_credentials(['https://www.googleapis.com/auth/drive.readonly'], credentials_file['client_id'], credentials_file['client_secret'])
+        except KeyError:
+            print('ERROR: Missing required keys (client_id or client_secret) in credential store.')
+            sys.exit(1)
+    else:
+        credentials = Credentials(**credentials_file)
+
+    # This requests Session object will handle token refresh and checking: https://google-auth.readthedocs.io/en/latest/reference/google.auth.transport.requests.html#google.auth.transport.requests.AuthorizedSession
     session = AuthorizedSession(credentials)
 
-    # https://developers.google.com/drive/api/v3/reference/files/list
-    # https://developers.google.com/drive/api/v3/search-files
-    files = session.get('%s/files?supportsAllDrives=true&includeItemsFromAllDrives=true&q=\'%s\' in parents and trashed = false' % (API_ROOT, PARENT_ID))
-    # if this doesn't work, a RefreshError will be raised the DC logo will be shown
+    # API: https://developers.google.com/drive/api/v3/reference/files/list
+    # Query structure: https://developers.google.com/drive/api/v3/search-files#query_string_examples
+    image_list = session.get(f'https://www.googleapis.com/drive/v3/files?supportsAllDrives=true&includeItemsFromAllDrives=true&q=\'{args.images_parent_id}\' in parents and trashed = false')
+    image_files = [file for file in image_list.json()['files'] if file['mimeType'].startswith('image')]  # Images only, please.
 
-    if not files.ok:
-        files.raise_for_status()
+    # Example /files response:
+    # {
+    #   "kind": "drive#fileList",
+    #   "incompleteSearch": false,
+    #   "files": [ ... ]
+    # }
 
-    # see if there are existing images; if so, remove the images directory and its content then create a new directory
-    if os.path.exists(IMAGES_PATH):
-        shutil.rmtree(IMAGES_PATH)
-    # we make the directory in both cases, whether it isn't there or when we remove it
-    os.mkdir(IMAGES_PATH)
+    images_dir = pathlib.Path('/tmp/gdrive-scroll/images')
+    # See if there are existing images; if so, remove the images directory and its content then create a new directory.
+    if images_dir.exists():
+        shutil.rmtree(images_dir)
+    images_dir.mkdir(parents=True)
 
-    # example file response:
-    #  {
-    #   "kind": "drive#file",
-    #   "id": "FOO",
-    #   "name": "FOO",
-    #   "mimeType": "image/jpeg"
-    #  }
-
-    # we don't want files that aren't images
-    images = [file for file in files.json()['files'] if file['mimeType'].startswith('image')]
-
-    for image in images:
+    image_paths = []
+    for image in image_files:
         # https://developers.google.com/drive/api/v3/reference/files/get
-        file = session.get('%s/files/%s?alt=media' % (API_ROOT, image['id']))
+        file = session.get(f'https://www.googleapis.com/drive/v3/files/{image["id"]}?alt=media')
 
-        with open('%s/%s' % (IMAGES_PATH, image['name']), 'wb') as f:
-            f.write(file.content)
+        # Example /files/ID response:
+        # {
+        #   "kind": "drive#file",
+        #   "id": "FOO",
+        #   "name": "FOO.jpg",
+        #   "mimeType": "image/jpeg"
+        # }
 
-    # store credentials so we don't have to re-authenticate next execution
-    with open(CREDENTIALS_PATH, 'wb') as f:
-        pickle.dump(credentials, f)
+        image_path = pathlib.Path(images_dir, image['name'])
+        with open(image_path, 'wb') as stream:
+            stream.write(file.content)
+        image_paths.append(image_path)
 
-    # collect images paths and start the scroll
-    image_paths = ['"%s/%s"' % (IMAGES_PATH, image['name']) for image in images]
-    subprocess.run('/usr/bin/fbi -noverbose -a -T 1 -t 10 %s' % ' '.join(image_paths), shell=True)
+    # subprocess.run('/usr/bin/fbi -noverbose -a -T 1 -t 10 %s' % ' '.join(image_paths), shell=True)
+
+    # Do almost everything again but for music this time.
+    if args.music_parent_id:
+        music_list = session.get(f'https://www.googleapis.com/drive/v3/files?supportsAllDrives=true&includeItemsFromAllDrives=true&q=\'{args.music_parent_id}\' in parents and trashed = false')
+        music_files = [file for file in music_list.json()['files'] if file['mimeType'].startswith('audio')]  # Audio only, please.
+        print(music_list)
+        print(music_files)
+
+        music_dir = pathlib.Path('/tmp/gdrive-scroll/music')
+        if music_dir.exists():
+            shutil.rmtree(music_dir)
+        music_dir.mkdir(parents=True)
+
+        music_paths = []
+        for music in music_files:
+            file = session.get(f'https://www.googleapis.com/drive/v3/files/{music["id"]}?alt=media')
+
+            music_path = pathlib.Path(music_dir, music['name'])
+            with open(music_path, 'wb') as stream:
+                stream.write(file.content)
+            music_paths.append(music_path)
+
+        # TODO: loop music
 
 
 if __name__ == '__main__':
-    try:
-        rolling_slides()
-    except Exception:  # if we hit any exception, just show the DC logo
-        subprocess.run('/usr/bin/fbi -noverbose -a -T 1 %s/dc.png' % DIR_PATH, shell=True)
+    main()
